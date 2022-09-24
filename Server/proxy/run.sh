7c1205e7c737788871b9ed0299ae5cc11ac2abcc
Anirataaak <stetter.fabian@regitstudios.de>
2022-09-24 23:04:51 +0200

    added run.sh

--- Server/proxy/run.sh
+++ Server/proxy/run.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-while true
-do
 sudo -Xms1G -Xmx1G --illegal-access=permit -jar proxy.jar
-for i in 5 4 3 2 1
-do
-  echo "Starte neu in $i"
-  sleep 1
-  done
-  echo "Startet!"
-  done
