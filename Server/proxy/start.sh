e9273bf1d40217da5262d707f9fabe16ef32fc76
Anirataaak <stetter.fabian@regitstudios.de>
2022-09-24 22:57:06 +0200

    edited proxy start.sh

--- Server/proxy/start.sh
+++ Server/proxy/start.sh
@@ -1,1 +1,12 @@
-sudo java -Xms1024M -Xmx1024M -jar proxy.jar -o true
+screen -d -m -S Proxy bash -c
+#!/bin/sh
+while true
+do
+sudo -Xms1G -Xmx1G --illegal-access=permit -jar proxy.jar
+for i in 5 4 3 2 1
+do
+  echo "Starte neu in $1"
+  sleep 1
+  done
+  echo "Startet!"
+  done
