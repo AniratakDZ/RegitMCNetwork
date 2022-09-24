8eaca3f18f49ec13e941430b54fb84a2b8473889
Anirataaak <stetter.fabian@regitstudios.de>
2022-09-24 23:01:05 +0200

    added run.sh

--- Server/proxy/start.sh
+++ Server/proxy/start.sh
@@ -1,12 +1,1 @@
-screen -d -m -S Proxy bash -c
-#!/bin/sh
-while true
-do
-sudo -Xms1G -Xmx1G --illegal-access=permit -jar proxy.jar
-for i in 5 4 3 2 1
-do
-  echo "Starte neu in $1"
-  sleep 1
-  done
-  echo "Startet!"
-  done
+screen -d -m -S Proxy bash -c 'cd /etc/mc/regitnetwork/proxy && ./run.sh'
